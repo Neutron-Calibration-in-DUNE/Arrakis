2b72e0a6c821d12079569ea33ad145c42a36891f
Nicholas Carrara <ncarrara.physics@gmail.com>
2022-07-07 11:25:29 -0700

    starting to implement arrays

--- ParticleTree.h
+++ ParticleTree.h
@@ -45,6 +45,35 @@
 #include "lardataobj/RecoBase/OpFlash.h"
 #include "larcoreobj/SimpleTypesAndConstants/geo_types.h"
 
+// special utility includes
+#include "fhiclcpp/types/Atom.h"
+#include "fhiclcpp/types/Table.h"
+#include "art_root_io/TFileService.h"
+
+// LArSoft includes
+#include "larcore/Geometry/Geometry.h"
+#include "larcorealg/Geometry/GeometryCore.h"
+#include "larcoreobj/SimpleTypesAndConstants/geo_types.h"
+#include "lardata/DetectorInfoServices/DetectorClocksService.h"
+#include "lardataobj/Simulation/SimEnergyDeposit.h"
+#include "lardataobj/RecoBase/PFParticle.h"
+#include "lardataobj/RecoBase/Cluster.h"
+#include "lardataobj/RecoBase/Hit.h"
+#include "lardataobj/RecoBase/Track.h"
+#include "lardataobj/RecoBase/SpacePoint.h"
+#include "lardataobj/RecoBase/Slice.h"
+#include "lardataobj/Simulation/SimChannel.h"
+#include "larsim/Simulation/LArG4Parameters.h"
+#include "nusimdata/SimulationBase/MCParticle.h"
+#include "nusimdata/SimulationBase/MCTruth.h"
+
+// necessary ROOT libraries
+#include <TTree.h>
+#include <TH1.h>
+#include "TH1F.h"
+#include "TGeoMaterial.h"
+#include "TGeoElement.h"
+
 namespace arrakis
 {
     class ParticleTree
