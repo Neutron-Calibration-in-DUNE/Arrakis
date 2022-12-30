312a6c301c1f582c5eba12af85cf63f2b11e79e1
Nicholas Carrara <ncarrara.physics@gmail.com>
2022-12-29 18:42:27 -0800

    fixing some errors

--- ParticleMaps.cxx
+++ ParticleMaps.cxx
@@ -51,9 +51,10 @@ namespace arrakis
     )
     {
         ResetEvent();
-        for(size_t ii = 0; ii < mcTruth.NParticles(); ii++)
+        for(size_t ii = 0; ii < mcParticles.size(); ii++)
         {
-            mOriginMap[mcTruth.GetParticle(ii).TrackId()] = mcTruth.Origin();
+            auto const& truth = mcTruth.at(ii);
+            mOriginMap[truth[0].GetParticle(0).TrackId()] = truth[0].Origin();
         }
         for (auto particle : *mcParticles)
         {
