de7dcbaa8db5b9346a6710846750ae932de2d9df
Nicholas Carrara <ncarrara.physics@gmail.com>
2023-01-02 08:59:09 -0800

    adjusting mc truth

--- Logger.h
+++ Logger.h
@@ -33,6 +33,10 @@ namespace arrakis
             return sName;
         }
 
+        std::string Date() const {
+            return sDate;
+        }
+
     protected:
         Logger(const std::string name);
         ~Logger() {}
@@ -47,25 +51,25 @@ namespace arrakis
         // instance
         static Logger* GetInstance(const std::string& name);
 
-        static std::string preamble()
+        std::string Preamble()
         {
             std::time_t now = std::time(0);
             std::tm *ltm = std::localtime(&now);
             std::string t = std::to_string(ltm->tm_hour) + ":" + std::to_string(ltm->tm_min) + ":" + std::to_string(ltm->tm_sec);
-            std::string preamble = "[" + sDate + " " + t + "] [" + sName + "] ";
-            return preamble
+            std::string preamble = "[" + Date() + " " + t + "] [" + Name() + "] ";
+            return preamble;
         }
 
         static void trace(std::string status)
         {
-            return preamble() + "[trace] " + status;
+            return Preamble() + "[trace] " + status;
         }
         
     private:
-        static std::string sYear = {""};
-        static std::string sMonth = {""};
-        static std::string sDay = {""};
-        static std::string sDate = {""};
+        static std::string sYear;
+        static std::string sMonth;
+        static std::string sDay;
+        static std::string sDate;
     };    
 }
 
