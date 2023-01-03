f0f6160ff237436eee0e802c8629bf996cc0ddc4
Nicholas Carrara <ncarrara.physics@gmail.com>
2023-01-03 08:51:02 -0800

    adjusting mc truth

--- SoloPointCloudGenerator.cxx
+++ SoloPointCloudGenerator.cxx
@@ -249,10 +249,10 @@ namespace arrakis
             if(gamma_channel.size() != 0)
             {
                 SoloPointCloud gamma_point_cloud;
-                gamma_point_cloud.channel = gamma_channel;
-                gamma_point_cloud.tick = gamma_tick;
-                gamma_point_cloud.adc = gamma_adc;
-                gamma_point_cloud.tdc = gamma_tdc;
+                gamma_point_cloud.channel = (std::vector<Double_t>)gamma_channel;
+                gamma_point_cloud.tick = (std::vector<Double_t>)gamma_tick;
+                gamma_point_cloud.adc = (std::vector<Double_t>)gamma_adc;
+                gamma_point_cloud.tdc = (std::vector<Double_t>)gamma_tdc;
                 gamma_point_cloud.point_cloud_id = mPointCloudID;
                 gamma_point_cloud.energy = capture_gamma_energy[ii];
 
@@ -284,10 +284,10 @@ namespace arrakis
         if(capture.channel.size() != 0)
         {
             SoloPointCloud capture_point_cloud;
-            capture_point_cloud.channel = capture_channel;
-            capture_point_cloud.tick = capture_tick;
-            capture_point_cloud.adc = capture_adc;
-            capture_point_cloud.tdc = capture_tdc;
+            capture_point_cloud.channel = (std::vector<Double_t>)capture_channel;
+            capture_point_cloud.tick = (std::vector<Double_t>)capture_tick;
+            capture_point_cloud.adc = (std::vector<Double_t>)capture_adc;
+            capture_point_cloud.tdc = (std::vector<Double_t>)capture_tdc;
             capture_point_cloud.point_cloud_id = mPointCloudID;
             mPointCloudID += 1;
             mSoloPointCloud = capture_point_cloud;
