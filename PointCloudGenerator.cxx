288bc04a578769525fa33b5f58507873b5e9b305
Nicholas Carrara <ncarrara.physics@gmail.com>
2023-01-31 05:16:24 -0800

    adc threshold added

--- PointCloudGenerator.cxx
+++ PointCloudGenerator.cxx
@@ -442,12 +442,12 @@ namespace arrakis
         junk_point_cloud.group_label = "junk";
         junk_point_cloud.group_label_id = -1;
         junk_point_cloud.label = "junk";
-        junk_point_cloud.view = junk.det_view;
-        junk_point_cloud.channel = junk.det_channel;
-        junk_point_cloud.wire = junk.det_wire;
-        junk_point_cloud.tick = junk.det_tick;
-        junk_point_cloud.adc = junk.det_adc;
-        junk_point_cloud.tdc = junk.det_tdc;
+        junk_point_cloud.view = (Double_t)junk.det_view;
+        junk_point_cloud.channel = (Double_t)junk.det_channel;
+        junk_point_cloud.wire = (Double_t)junk.det_wire;
+        junk_point_cloud.tick = (Double_t)junk.det_tick;
+        junk_point_cloud.adc = (Double_t)junk.det_adc;
+        junk_point_cloud.tdc = (Double_t)junk.det_tdc;
         junk_point_cloud.point_cloud_id = mPointCloudID;
         junk_point_cloud.total_energy = 0;
         mSoloPointClouds.emplace_back(junk_point_cloud);
