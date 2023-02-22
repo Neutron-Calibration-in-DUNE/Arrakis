b3860b999ed850c4dff455ffedff9b48cac575f9
Nicholas Carrara <ncarrara.physics@gmail.com>
2023-02-22 08:33:21 -0800

    some changes

--- MCData.h
+++ MCData.h
@@ -67,13 +67,6 @@ namespace arrakis
             art::Handle<std::vector<sim::SimChannel>>       mMCSimChannelHandle;
             art::Handle<std::vector<raw::RawDigit>>         mMCRawDigitHandle;
 
-            // valid handles
-            art::ValidHandle<std::vector<simb::MCTruth>>         mMCTruthValidHandle;
-            art::ValidHandle<std::vector<simb::MCParticle>>      mMCParticleValidHandle;
-            art::ValidHandle<std::vector<sim::SimEnergyDeposit>> mMCSimEnergyDepositValidHandle;
-            art::ValidHandle<std::vector<sim::SimChannel>>       mMCSimChannelValidHandle;
-            art::ValidHandle<std::vector<raw::RawDigit>>         mMCRawDigitValidHandle;
-
         };
     }
 }
