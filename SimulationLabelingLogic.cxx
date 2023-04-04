0d576d39ea3ef07515366b5f24d7257f31063aa2
Nicholas Carrara <ncarrara.physics@gmail.com>
2023-04-04 08:50:29 -0700

    some changes

--- SimulationLabelingLogic.cxx
+++ SimulationLabelingLogic.cxx
@@ -44,7 +44,7 @@ namespace arrakis
         Logger::GetInstance("SimulationLabelingLogic")->trace(
             "setting up configuration parameters.");
 
-        sFilterDetectorSimulation = std::to_string(config().FilterDetectorSimulation());
+        sFilterDetectorSimulation = config().FilterDetectorSimulation().c_str();
         Logger::GetInstance("SimulationLabelingLogic")->trace(
             "setting filter detector simulation to: " + sFilterDetectorSimulation
         );
